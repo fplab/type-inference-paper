7b7b79f4c2d24ea6be2d545620e8540440aefd46
Zoe <zhiyipan@umich.edu>
2020-06-09 23:16:04 -0400

    finish implemention draft

--- code/impl.ml
+++ code/impl.ml
@@ -5,13 +5,17 @@ module Identifier = struct
   type t = string
 end
 
+module TypeInferenceVar = struct
+  type t = int
+  let recent (var_1:t) (var_2:t) = max var_1 var_2;; 
+end
 module Typ = struct
-  type hole_id = int
   type t =
-    | THole of hole_id
+    | THole of TypeInferenceVar.t
     | TNum
     | TArrow of t * t
 
+  type subs = (TypeInferenceVar.t * t) list
   let type_variable = ref (0)
 
   (* generates a new unique type variable *)
@@ -19,6 +23,15 @@ module Typ = struct
       let var = !type_variable in
       incr type_variable; var
     ;;
+  
+  let rec load_type_variable (t: t) =
+    match t with
+    | THole id -> type_variable:= TypeInferenceVar.recent id !type_variable
+    | TNum -> ()
+    | TArrow (ty1,ty2)-> (
+      load_type_variable(ty1);
+      load_type_variable(ty2);
+    )
 end
 
 module Exp = struct
@@ -75,34 +88,60 @@ end
 (* TBD: build map for fresh var? *)
 let get_match_arrow_typ (t: Typ.t): Typ.t option * Constraints.t = 
   match t with
-  | Thole _ -> (
-    let var_in = gen_new_type_var() in 
-    let var_out = gen_new_type_var() in
-    let arrow_typ = TArrow (THole(var_in),THole(var_out)) in
+  | THole _ -> (
+    let var_in = Typ.gen_new_type_var() in 
+    let var_out = Typ.gen_new_type_var() in
+    let arrow_typ = Typ.TArrow (THole(var_in),THole(var_out)) in
     (Some arrow_typ,[(t,arrow_typ)])
     )
   | TArrow (_,_) -> (Some t, [])
   | _ -> (None, [])
 
+
+let rec update_type_variable (ctx: Ctx.t) (e: Exp.t): unit =
+  match e with
+  | EVar x ->(
+    match Ctx.lookup ctx x with
+    | None -> ();
+    | Some(typ) -> Typ.load_type_variable typ;
+    )
+  | ELam (_, exp) -> update_type_variable ctx exp;
+  | ELamAnn (_, ty_in, exp) -> (
+    Typ.load_type_variable(ty_in);
+    update_type_variable ctx exp;
+  )
+  | EBinOp (e1, _, e2) -> (
+    update_type_variable ctx e1;
+    update_type_variable ctx e2;
+  )
+  | ENumLiteral _ -> ()
+  | EAsc (exp, typ) -> (
+    Typ.load_type_variable(typ);
+    update_type_variable ctx exp;
+  )
+  | EEmptyHole _
+  | EExpHole _ -> ()
+
 let rec syn (ctx: Ctx.t) (e: Exp.t): Typ.t option * Constraints.t =
+  (* update_type_variable ctx e; *)
   match e with
   | EVar x -> (Ctx.lookup ctx x, [])
-  | ELam (_, _) -> (None,[]) (* TBD *)
+  | ELam (_, _) -> (None,[]) 
   | ELamAnn (x, ty1, e2) -> (
       match syn (Ctx.extend ctx (x, ty1)) e2 with
-      | (None, _)-> (None, []) (* TBD *)
+      | (None, cons)-> (None, cons) 
       | (Some ty2, cons) -> (Some (TArrow (ty1, ty2)),cons) )
   | EBinOp (e1, OpPlus, e2) -> (
       match (ana ctx e1 Typ.TNum, ana ctx e2 Typ.TNum) with
-      | ((false,_), _) 
-      | (_, (false,_)) -> (None,[]) (* TBD *)
+      | ((false, cons1), (_, cons2)) 
+      | ((_, cons1), (false, cons2)) -> (None,cons1@cons2)
       | ((true,cons1), (true,cons2)) -> (Some TNum, cons1@cons2) )
   | EBinOp (e1, OpAp, e2) -> (
       match (syn ctx e1) with
-      | (None, _)-> (None, []) (* TBD *)
+      | (None, cons)-> (None, cons) (* TBD *)
       | (Some(typ_e1), cons1) -> (
           match get_match_arrow_typ typ_e1 with
-          | (None, _) -> (None, []) (* TBD *)
+          | (None, cons2) -> (None, cons1@cons2) (* TBD *)
           | (Some(TArrow (ty_in, ty_out)), cons2) -> (
             match ana ctx e2 ty_in with
             | (false, _) -> (None, []) (* TBD *)
@@ -111,37 +150,39 @@ let rec syn (ctx: Ctx.t) (e: Exp.t): Typ.t option * Constraints.t =
           | _ -> raise Impossible
         )
     )
-  | ENumLiteral _ -> (Some TNum, cons)
+  | ENumLiteral _ -> (Some TNum, [])
   | EAsc (exp, typ) -> (
       match ana ctx exp typ with
       | (false, _) -> (None, []) (* TBD *)
       | (true, cons) -> (Some typ, cons)
   )
-  | EEmptyHole _ -> (Some THole(gen_new_type_var()),[])
+  | EEmptyHole _ -> (Some (THole (Typ.gen_new_type_var())),[])
   | EExpHole (_, e2) -> (
     match syn ctx e2 with
     | (None, _)-> (None, []) (* TBD *)
-    | (Some _, cons) -> (Some THole(gen_new_type_var()),cons)
+    | (Some _, cons) -> (Some (THole (Typ.gen_new_type_var())),cons)
   )
-and ana (ctx: Ctx.t) (e: Exp.t) (ty: Typ.t): bool =
+and ana (ctx: Ctx.t) (e: Exp.t) (ty: Typ.t): bool * Constraints.t =
   match e with
   | ELam (x, exp) -> (
     match get_match_arrow_typ ty with
-    | (None, _) -> (None, []) (* TBD *)
+    | (None, _) -> (false, []) (* TBD *)
     | (Some(TArrow (ty_in, ty_out)), cons1) -> (
       match ana (Ctx.extend ctx (x, ty_in)) exp ty_out with
       | (false, _) -> (false, []) (* TBD *)
       | (true, cons2) -> (true, cons1@cons2)
     )
+    | _ -> raise Impossible
   ) 
   | ELamAnn (x, ty_in', exp) -> (
     match get_match_arrow_typ ty with
-    | (None, _) -> (None, []) (* TBD *)
+    | (None, _) -> (false, []) (* TBD *)
     | (Some(TArrow (ty_in, ty_out)), cons1) -> (
       match ana (Ctx.extend ctx (x, ty_in')) exp ty_out with
       | (false, _) -> (false, []) (* TBD *)
       | (true, cons2) -> (true, cons1@cons2@[(ty_in,ty_in')])
     )
+    | _ -> raise Impossible
   ) 
   | EVar _
   | EBinOp _ 
@@ -151,6 +192,37 @@ and ana (ctx: Ctx.t) (e: Exp.t) (ty: Typ.t): bool =
   | EExpHole _ ->
     (* subsumption *)
       (match syn ctx e with
-        | (None, _) -> (None, []) (* TBD *)
+        | (None, _) -> (false, []) (* TBD *)
         | (Some ty', cons) -> (true, cons@[(ty,ty')])
       )
+;;
+  
+let rec substitute (u: Typ.t) (x: TypeInferenceVar.t) (t: Typ.t) : Typ.t =
+  match t with
+  | TNum -> t
+  | THole v -> if v = x then u else t
+  | TArrow(t1, t2) -> TArrow(substitute u x t1, substitute u x t2)
+;;
+
+let apply (subs: Typ.subs) (t: Typ.t) : Typ.t =
+  List.fold_right (fun (x, u) t -> substitute u x t) subs t
+;;
+
+let rec unify (constraints: Constraints.t) : Typ.subs option =
+  match constraints with
+  | [] -> Some []
+  | (x, y) :: xs ->
+    match unify xs with
+    | None -> None
+    | Some subs1 -> (
+      match unify_one (apply subs1 x) (apply subs1 y) with
+      | None -> None
+      | Some subs2 -> Some (subs1@subs2)
+    )
+and unify_one (t1: Typ.t) (t2: Typ.t) : Typ.subs option =
+    match (t1, t2) with
+    | (TNum, TNum) -> Some []
+    | (THole x, z) | (z, THole x) -> Some [(x, z)]
+    | (TArrow(a, b), TArrow(x, y)) -> unify [(a, x); (b, y)]
+    | _ -> None
+  ;;
