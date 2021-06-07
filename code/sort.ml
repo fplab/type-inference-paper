368e008fb2a10a2e6f04fec4a98b0df639dc4c35
Raef Maroof <maroofr@umich.edu>
2021-06-07 15:57:50 -0400

    edited code so it compiles

--- code/sort.ml
+++ code/sort.ml
@@ -37,9 +37,9 @@ to be reported for such variables is TBD *)
 let inf_var_is_depended_upon (var: TypeInferenceVar.t) (result_list: Typ.unify_result list)
     : bool =
     let result_contains_var (result: Typ.unify_result): bool =
-        match ty with
-        | Solved ty -> (Typ.contains_var ty)
-        | Unsolved tys -> (List.exists Typ.contains_var tys)
+        match result with
+        | Solved ty -> (Typ.contains_var var ty)
+        | UnSolved tys -> (List.exists (Typ.contains_var var) tys)
     in
     List.exists result_contains_var result_list
 ;;
@@ -49,10 +49,9 @@ let rec retrieve_result_for_inf_var (var: TypeInferenceVar.t) (results: Typ.unif
     : Typ.unify_result option =
     match results with
     | [] -> None
-    | ((THole ty_var), result)::tl -> (
+    | (ty_var, result)::tl -> (
         if (ty_var == var) then (Some result) else retrieve_result_for_inf_var var tl
     )
-    | _::tl -> retrieve_result_for_inf_var var tl
 ;;
 
 (* Searches for instances of target within the type's tree structure and replaces if found *)
@@ -65,105 +64,6 @@ let rec find_and_replace (target: TypeInferenceVar.t) (replacement: Typ.t) (ty: 
     | TSum (ty1, ty2) -> TSum ((replace_target_in ty1), (replace_target_in ty2))
     | THole ty_var -> if (ty_var == target) then replacement else ty
     | _ -> ty
-;;
-
-(*Performs a topological sort on the unify results by interpreting it as an adjacency list*)
-(*Performs substitution in order based on type dependencies *)
-let top_sort_and_sub (results: Typ.unify_results)
-    : Typ.unify_results = 
-    (*Find roots; a root corresponds to a variable that no variables are dependent on (no incoming edges)*)
-    let var_list = Typ.extract_var_list results in
-    let result_list =  Typ.extract_result_list results in
-    let tuple_dependencies (var: TypeInferenceVar.t): (TypeInferenceVar.t * bool) = 
-        (var, (inf_var_is_depended_upon var result_list))
-    in
-    let vars_with_dependency = List.map tuple_dependencies var_list in
-    let wrap_not_depended (tuple: TypeInferenceVar.t * bool): Typ.t option =
-        match tuple with
-        | (var, true) -> None
-        | (var, false) -> Some(THole var)
-    in
-    (*generate the root list from all variables that are not depended upon by filtering None's*)
-    let root_list = List.filter_map wrap_not_depended vars_with_dependency in
-    (*update the unify_results by successively passing its current state to be resolved by substitution along each root node*)
-    (*folding sub on root is a wrapped version of sub_on_root_by_dependence for fold_left*)
-    let folding_sub_on_root (acc_res: Typ.unify_results) (root: Typ.t): Typ.unify_results =
-        let (results, _) = sub_on_root_by_dependence acc_res root in
-        results
-    in
-    let results = List.fold_left folding_sub_on_root results root_list in
-    results
-;;
-
-(*recurses on the root node specified and adjusts each var solution to its most basic (most independent/literal) value*)
-(*each call returns the current adjusted set of results and the unify_result associated with the recursed upon node *)
-(*if a node's children are known to be inconsistent, all dependent nodes will also be rendered UnSolved *)
-(*
-ex:     THole 0 = Solved THole 1
-        THole 1 =  UnSolved TNum TBool
-    will resolve to
-        THole 0 = UnSolved TNum TBool
-        THole 1 = UnSolved TNum TBool
-*)
-let rec sub_on_root_by_dependence (results: Typ.unify_results) (root: Typ.t)
-    : (Typ.unify_results * Typ.unify_result) =
-    match root with
-    | TBool -> (results, TBool)
-    | TNum -> (results, TNum)
-    | TArrow (ty1, ty2) -> sub_two_of_constructor Typ.mk_arrow ty1 ty2
-    | TProd (ty1, ty2) -> sub_two_of_constructor Typ.mk_prod ty1 ty2
-    | TSum (ty1, ty2) -> sub_two_of_constructor Typ.mk_sum ty1 ty2
-    | THole var -> (
-        match (retrieve_result_for_inf_var var results) with
-        | Some unif_res -> (
-            match unif_res with
-            | Solved ty -> (
-                let (results, result_ty) = sub_on_root_by_dependence results ty in
-                ((sub_inf_var_for_child var result_ty results), result_ty)
-            )
-            | Unsolved tys -> (
-                (*the following function accumulates the current state of the unify results and list set
-                by taking the current state and a new child's type and updating the state by recursing on the type *)
-                let recurse_and_accumulate (acc: (Typ.unify_results * (Typ.t list))) (ty: Typ.t)
-                    : (Typ.unify_results * (Typ.t list)) =
-                    let (curr_results, curr_list) = acc in
-                    let (updated_results, unify_res) = sub_on_root_by_dependence results ty in
-                    let ty_results = 
-                        match unify_res with
-                        | Solved single_ty -> [single_ty]
-                        | UnSolved tys -> tys
-                    in
-                    (updated_results, ty_results @ curr_list)
-                in
-                let (results, inconsistency_list) = List.fold_left recurse_and_acc (results, []) tys in
-                let child_res =  UnSolved inconsistency_list in
-                ((sub_inf_var_for_child var child_res results), child_res)
-            )
-        )
-        | None -> raise Impossible (* list of unification results itself was used to generate variable names used; must be present *)
-    )
-(* a common instance for recursive types *)
-and sub_two_of_constructor (ctr: (Typ.t -> Typ.t) -> Typ.t) (ty1: Typ.t) (ty2: Typ.t)
-    : (Typ.unify_results * Typ.unify_result) =
-    let (results, result_ty1) = sub_on_root_by_dependence results ty1 in
-    let (results, result_ty2) = sub_on_root_by_dependence results ty2 in
-    let mk_ctr_types (ctr: (Typ.t -> Typ.t) -> Typ.t) (const: Typ.t) (const_is_left: bool) (acc: Typ.t list) (variant: Typ.t)
-        : Typ.t list =
-        if (const_is_left) then (ctr const varaint)::acc else (ctr variant const)::acc
-    in
-    let updated_unify_result =
-        match (result_ty1, result_ty2) with
-        | ((Solved resolved_ty1), (Solved resolved_ty2)) -> Solved (ctr resolved_ty1 resolved_ty2)
-        | ((UnSolved tys), (Solved resolved_ty2)) -> UnSolved (List.fold_left (mk_ctr_types ctr resolved_ty2 false) [] tys)
-        | ((Solved resolved_ty1), (UnSolved tys)) -> Unsolved (List.fold_left (mk_ctr_types ctr resolved_ty1 true) [] tys)
-        | ((UnSolved tys1), (UnSolved tys2)) -> (
-            let acc_mk_ctr_types (acc: Typ.t list) (const_of_left: Typ.t): Typ.t list = 
-                List.fold_left (mk_ctr_types ctr const_of_left true) acc tys2
-            in
-            UnSolved (List.fold_left acc_mk_ctr_types [] tys1)
-        )
-    in
-    (results, updated_unify_result)
 ;;
 
 (*Iterates through unify_results to replace all instances of target with ty. 
@@ -193,10 +93,10 @@ let sub_inf_var_for_child (target: TypeInferenceVar.t) (child: Typ.unify_result)
                         (find_and_replace target replacement_ty base_ty)::acc_s
                     in
                     (*for efficiency in empty accumulator case*)
-                    let extended = (List.fold_left accumulate_unsolved_by_single (false, acc_list) replacements) in
+                    let extended = List.fold_left accumulate_unsolved_by_single acc_list replacements in
                     (true, extended)
                 ) else (
-                    (changed || false, base_ty::acc)
+                    (changed || false, base_ty::acc_list)
                 )
             in
             (*match on the result associated with the variable 
@@ -207,7 +107,7 @@ let sub_inf_var_for_child (target: TypeInferenceVar.t) (child: Typ.unify_result)
                 match child with
                 | Solved child_ty -> (var, Solved (find_and_replace target child_ty var_ty)) 
                 | UnSolved child_tys -> (
-                    let (changed, sub_list) = accumulate_unsolved_by_list child_tys [] var_ty in
+                    let (changed, sub_list) = accumulate_unsolved_by_list child_tys (false, []) var_ty in
                     if (changed) then (var, UnSolved (sub_list)) else var_with_res
                 )
             )
@@ -232,6 +132,105 @@ let sub_inf_var_for_child (target: TypeInferenceVar.t) (child: Typ.unify_result)
     in
     (*given how large sub_on_res is, it may be a bad idea to use it non tail recursively. hence, the rev method is used *)
     List.rev(List.rev_map sub_on_res results)
+;;
+
+(*recurses on the root node specified and adjusts each var solution to its most basic (most independent/literal) value*)
+(*each call returns the current adjusted set of results and the unify_result associated with the recursed upon node *)
+(*if a node's children are known to be inconsistent, all dependent nodes will also be rendered UnSolved *)
+(*
+ex:     THole 0 = Solved THole 1
+        THole 1 =  UnSolved TNum TBool
+    will resolve to
+        THole 0 = UnSolved TNum TBool
+        THole 1 = UnSolved TNum TBool
+*)
+let rec sub_on_root_by_dependence (root: Typ.t) (results: Typ.unify_results)
+    : (Typ.unify_results * Typ.unify_result) =
+    match root with
+    | TBool -> (results, Solved TBool)
+    | TNum -> (results, Solved TNum)
+    | TArrow (ty1, ty2) -> sub_two_of_constructor Typ.mk_arrow ty1 ty2 results
+    | TProd (ty1, ty2) -> sub_two_of_constructor Typ.mk_prod ty1 ty2 results
+    | TSum (ty1, ty2) -> sub_two_of_constructor Typ.mk_sum ty1 ty2 results
+    | THole var -> (
+        match (retrieve_result_for_inf_var var results) with
+        | Some unif_res -> (
+            match unif_res with
+            | Solved ty -> (
+                let (results, result_ty) = sub_on_root_by_dependence ty results in
+                ((sub_inf_var_for_child var result_ty results), result_ty)
+            )
+            | UnSolved tys -> (
+                (*the following function accumulates the current state of the unify results and list set
+                by taking the current state and a new child's type and updating the state by recursing on the type *)
+                let recurse_and_accumulate (acc: (Typ.unify_results * (Typ.t list))) (ty: Typ.t)
+                    : (Typ.unify_results * (Typ.t list)) =
+                    let (curr_results, curr_list) = acc in
+                    let (updated_results, unify_res) = sub_on_root_by_dependence ty curr_results in
+                    let ty_results = 
+                        match unify_res with
+                        | Solved single_ty -> [single_ty]
+                        | UnSolved tys -> tys
+                    in
+                    (updated_results, ty_results @ curr_list)
+                in
+                let (results, inconsistency_list) = List.fold_left recurse_and_accumulate (results, []) tys in
+                let child_res : Typ.unify_result =  UnSolved inconsistency_list in
+                ((sub_inf_var_for_child var child_res results), child_res)
+            )
+        )
+        | None -> raise Impossible (* list of unification results itself was used to generate variable names used; must be present *)
+    )
+(* a common instance for recursive types *)
+and sub_two_of_constructor (ctr: Typ.t -> Typ.t -> Typ.t) (ty1: Typ.t) (ty2: Typ.t) (results: Typ.unify_results)
+    : (Typ.unify_results * Typ.unify_result) =
+    let (results, result_ty1) = sub_on_root_by_dependence ty1 results in
+    let (results, result_ty2) = sub_on_root_by_dependence ty2 results in
+    let mk_ctr_types (ctr: Typ.t -> Typ.t -> Typ.t) (const: Typ.t) (const_is_left: bool) (acc: Typ.t list) (variant: Typ.t)
+        : Typ.t list =
+        if (const_is_left) then (ctr const variant)::acc else (ctr variant const)::acc
+    in
+    let updated_unify_result : Typ.unify_result =
+        match (result_ty1, result_ty2) with
+        | ((Solved resolved_ty1), (Solved resolved_ty2)) -> Solved (ctr resolved_ty1 resolved_ty2)
+        | ((UnSolved tys), (Solved resolved_ty2)) -> UnSolved (List.fold_left (mk_ctr_types ctr resolved_ty2 false) [] tys)
+        | ((Solved resolved_ty1), (UnSolved tys)) -> UnSolved (List.fold_left (mk_ctr_types ctr resolved_ty1 true) [] tys)
+        | ((UnSolved tys1), (UnSolved tys2)) -> (
+            let acc_mk_ctr_types (acc: Typ.t list) (const_of_left: Typ.t): Typ.t list = 
+                List.fold_left (mk_ctr_types ctr const_of_left true) acc tys2
+            in
+            UnSolved (List.fold_left acc_mk_ctr_types [] tys1)
+        )
+    in
+    (results, updated_unify_result)
+;;
+
+(*Performs a topological sort on the unify results by interpreting it as an adjacency list*)
+(*Performs substitution in order based on type dependencies *)
+let top_sort_and_sub (results: Typ.unify_results)
+    : Typ.unify_results = 
+    (*Find roots; a root corresponds to a variable that no variables are dependent on (no incoming edges)*)
+    let var_list = Typ.extract_var_list results in
+    let result_list =  Typ.extract_result_list results in
+    let tuple_dependencies (var: TypeInferenceVar.t): (TypeInferenceVar.t * bool) = 
+        (var, (inf_var_is_depended_upon var result_list))
+    in
+    let vars_with_dependency = List.map tuple_dependencies var_list in
+    let wrap_not_depended (tuple: TypeInferenceVar.t * bool): Typ.t option =
+        match tuple with
+        | (_, true) -> None
+        | (var, false) -> Some(THole var)
+    in
+    (*generate the root list from all variables that are not depended upon by filtering None's*)
+    let root_list = List.filter_map wrap_not_depended vars_with_dependency in
+    (*update the unify_results by successively passing its current state to be resolved by substitution along each root node*)
+    (*folding sub on root is a wrapped version of sub_on_root_by_dependence for fold_left*)
+    let folding_sub_on_root (acc_res: Typ.unify_results) (root: Typ.t): Typ.unify_results =
+        let (results, _) = sub_on_root_by_dependence root acc_res in
+        results
+    in
+    let results = List.fold_left folding_sub_on_root results root_list in
+    results
 ;;
 
 (*The following three functions do not seem to have use anymore:
@@ -261,6 +260,6 @@ let smallest_inconsistent_set (list_set: (Typ.t list) list)
     match list_set with
     | [] -> []
     | hd::tl -> 
-        let tl = List.fold_left tl [] (@) in
+        let tl = List.fold_left (@) [] tl in
         smallest_inconsistent_pair hd tl
 ;;
